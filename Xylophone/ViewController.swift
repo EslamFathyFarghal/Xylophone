938e7e96b5f4b9d6f71ceca78791317222b18b8c
Eslam Fathy <eslmafathyfarghal@gmail.com>
2020-04-14 13:57:30 +0200

    working Xylophone

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func cPressed(_ sender: UIButton) {
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() +  0.2){
+            sender.alpha = 1.0
+        }
+        playSound(sender: sender.currentTitle!)
+    }
     
-
+    func playSound(sender : String) {
+        let url = Bundle.main.url(forResource: sender , withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
